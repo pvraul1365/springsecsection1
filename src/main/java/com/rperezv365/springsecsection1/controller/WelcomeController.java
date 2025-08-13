711b6a128b6e13611cf53d022238c1b21234584e
Raúl Pérez Vicente <rperezv@ayesa.com>
2025-08-13 11:45:27 +0200

    5. Creating a simple Spring Boot app with out security - Part 2

---
+++ src/main/java/com/rperezv365/springsecsection1/controller/WelcomeController.java
@@ -0,0 +1,22 @@
+package com.rperezv365.springsecsection1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * WelcomeController
+ * <p>
+ * Created by IntelliJ, Spring Framework Guru.
+ *
+ * @author architecture - pvraul
+ * @version 13/08/2025 - 11:33
+ * @since 1.17
+ */
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String sayWelcome() {
+        return "Welcome to Spring Application with out security";
+    }
+}
